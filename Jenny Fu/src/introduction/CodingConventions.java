e6a0042aa428ffde7f2a2fcf399a7cdc8d280746
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-09-13 10:58:03 -0400

    coding conventions lesson

--- Jenny Fu/src/introduction/CodingConventions.java
+++ Jenny Fu/src/introduction/CodingConventions.java
@@ -5,16 +5,26 @@ public class CodingConventions {
 	private String name;
 	private String description;
 	
-	
 	/*
 		this is a special method, a "constructor"
 		Notice it does not have a return type
 		That is because the thing that is being returned is an instance of a class.
 		Therefore, the name for the instructor must always match the class.
 	*/
-	public CodingConventions() {
+	public CodingConventions(String name, int index) {
 		//In Java, fields are instantiated in a constructor
-		name = "Jenny";
-		description = "is a student";
+		/*
+			there is a local variable called 'name' and a field called 'name'
+			the field persists but the local variable data is destroyed at
+			the end of this method distinguish the field by using the reserved word 'this'
+		*/
+		this.name = name;
+		description = IntroMain.DESCRIPTIONS[index];
+	}
+	
+	public void doStuff() {
+		//use a space between your operations
+		System.out.println(name + description);
+		System.out.println("The square root of 12 is " + Math.sqrt(12));;
 	}
 }
--- Jenny Fu/src/introduction/IntroMain.java
+++ Jenny Fu/src/introduction/IntroMain.java
@@ -2,12 +2,21 @@ package introduction; //this is a package declaration
 
 //this is class header (class declaration)
 public class IntroMain {
+	
+	//constants (public)
+	public static final String[] DESCRIPTIONS = {" is a teacher",
+			" is a student",
+			" is a human",
+			" is imaginary"};
 
 	public static void main(String[] args) {
 		//declaration of a local variable
 		//notice the use of the word 'new'
 		//new must be used to call a constructor
-		CodingConventions conventionsInstance = new CodingConventions();
+		CodingConventions conventionsInstance = new CodingConventions("Mr. Nockles", 0);
+		
+		//instance method call
+		conventionsInstance.doStuff();
 	}
 
 }
