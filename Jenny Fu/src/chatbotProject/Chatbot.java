e2b1af72683d337df31cccb94244c4b99f3ea411
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-09-28 11:44:29 -0400

    bot

--- Jenny Fu/src/chatbotProject/Chatbot.java
+++ Jenny Fu/src/chatbotProject/Chatbot.java
@@ -1,6 +1,30 @@
 package chatbotProject;
 
 
-public class Chatbot implements Topic {
+public class Chatbot {
+	private String userName;
+	private Topic jenny;
+	private boolean chatting;
+
+	public Chatbot() {
+		jenny = new ChatbotJenny();
+		userName = "unknown user";
+		chatting = true;
+	}
+	
+	public void startTalking() {
+		ChatbotMain.print("Welcome to our chatbot! What is your name?");
+		userName = ChatbotMain.getInput();
+		while(chatting) {
+			ChatbotMain.print("What do you want to talk about?");
+			String response = ChatbotMain.getInput();
+			if(jenny.isTriggered(response)) {
+				chatting = false;
+				jenny.startChatting();
+			}
+			else
+				ChatbotMain.print("I'm sorry. I don't understand.");
+		}
+	}
 
 }
