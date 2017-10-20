cc4cb5f32b0a9e76665d01271a0d98cbb209ad26
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-10-20 11:20:21 -0400

    ad

--- Jenny Fu/src/arrays/Main.java
+++ Jenny Fu/src/arrays/Main.java
@@ -1,9 +1,48 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
-		ObjectArrays oA = new ObjectArrays();
+		//ObjectArrays oA = new ObjectArrays();
+		Person s = new Person("Jenny", "Fu", Borough.NY_BOROUGHS[0]);
+		int x = 10;
+		int[] a = {5,6,7};
+		test2(s,x,a);
+		System.out.println(s + ", " + x + ", " + Arrays.toString(a));
 	}
+	
+	/**
+	 * THIS CHANGES THE NAME AND ARRAY
+	 * 		-NAME CHANGES WITH FIELDS
+	 * 		-ARRAY CHANGES WITH INDEXES (because the indexes of an array are references)
+	 * 
+	 * NO VOID METHOD CAN PERMANENTLY CHANGE AN INT
+	 * (you can set it as a return type)
+	 */
+	public static void test2(Person s, int x, int[] arr) {
+		s.setFirstName("Qibing");
+		x = 5;
+		//arr = new int[3];
+		//AS LONG AS YOU DONT MAKE A NEW ARRAY, THE REFERENCES THEMSELVES WILL CHANGE.
+		arr[0] = 1;
+		arr[1] = 2;
+		arr[2] = 3;
+	}
+	
+	/**
+	 * PASS-BY-VALUE EXAMPLES
+	 * DOES NOT CHANGE ANYTHING
+	 */
 
+	public static void test(Person s, int x, int[] arr) {
+		String name = s.getFirstName();
+		name = "Qibing";
+		x = 5;
+		arr = new int[3];
+		arr[0] = 1;
+		arr[1] = 2;
+		arr[2] = 3;
+	}
 }
--- Jenny Fu/src/arrays/Person.java
+++ Jenny Fu/src/arrays/Person.java
@@ -1,7 +1,7 @@
 package arrays;
 
 public class Person {
-	
+
 	public static final String[] FIRST_START = {"Chr", "M", "L", "Gr", "Ph", "Th"};
 	public static final String[] FIRST_MIDDLE = {"isti", "icha", "era", "eta", "ala", "ina", "ara"};
 	public static final String[] FIRST_END = {"", "na","r","n", "tian", "rs", "mp", "les", "y"};
@@ -9,21 +9,60 @@ public class Person {
 	public static final String[] LAST_START = {"Tr", "Br", "L", "Gr", "Sh", "B", "Th"};
 	public static final String[] LAST_MIDDLE = {"on", "o", "an", "ola", "et", "e", "is"};
 	public static final String[] LAST_END = {"", "son","ers","r", "tian", "ston", "ck"};
-	
+
 	private String firstName;
 	private String lastName;
 	private Borough home;
 	private Hobby hobby;
 	private Person[] friends;
-	
+	private String nickname;
+
 	public Person(String first, String last, Borough home) {
 		firstName = first;
 		lastName = last;
 		this.home = home;
 		this.hobby = Hobby.randomHobby();
 		friends = new Person[3];
+		this.nickname = createNickname(firstName);
 	}
 	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+		nickname = createNickname(firstName);
+	}
+	
+
+	//Java is PASS-BY-VALUE, meaning the parameters of the method are just values, not references.
+	//So if you change those values, the original object is not affected.
+	//In this case, name will not be changed. In fact, NOTHING can change this.firstName via name.
+	
+	public static String createNickname(String name) {
+		String nickname = "";
+		int sec = findSecond(name);
+		nickname = name.substring(0, sec);
+		return nickname;
+	}
+
+	public static int findSecond(String str) {
+		int idx = str.length();
+		boolean foundVowel = false;
+		for(int i = 0; i < str.length(); i++) {
+			if(str.toLowerCase().substring(i, i+ 1).equals("a") || str.toLowerCase().substring(i, i+ 1).equals("e") || str.toLowerCase().substring(i, i+ 1).equals("i") || str.toLowerCase().substring(i, i+ 1).equals("o") || str.toLowerCase().substring(i, i+ 1).equals("a") || str.toLowerCase().substring(i, i+ 1).equals("u")) {
+				if(!foundVowel) {
+					foundVowel = true;
+				}else {
+					return i;
+				}
+			}
+		}
+		return idx;
+	}
+
 	//chooses friends from people based on who is of same class as this instance and who has the same hobbies.
 	public void mingle(Person[] people) {
 		for(Person p: people) {
@@ -34,31 +73,31 @@ public class Person {
 			}
 		}
 	}
-	
+
 	public Person betterFriend(Person p, Person q) {
 		if(p == null) {
 			return q;
 		}
-		
+
 		if(q == null) {
 			return p;
 		}
-		
+
 		if(p.getClass() == this.getClass() && q.getClass() == this.getClass()) {
 			if(p.hobby == this.hobby)
 				return p;
 			if(q.hobby == this.hobby)
 				return q;
 		}
-		
+
 		if(p.getClass() == this.getClass()) {
 			return p;
 		}
-		
+
 		if(q.getClass() == this.getClass()) {
 			return q;
 		}
-		
+
 		//if none of these are true, just take p.
 		return p;
 	}
@@ -70,7 +109,7 @@ public class Person {
 		}
 		friends[0] = p;
 	}
-	
+
 	public void printFriends() {
 		System.out.println("My name is " + firstName + " " + lastName + " and these are my friends:");
 		for(Person f: friends) {
@@ -78,8 +117,8 @@ public class Person {
 				System.out.println(f);
 		}
 	}
-	
+
 	public String toString() {
-		return "My name is " + firstName + " " + lastName + " and I live in " + home + ". I like " + hobby + ".";
+		return "My name is " + firstName + " " + lastName + " and I live in " + home + ". Call me " + nickname + ".\nI like " + hobby + ".";
 	}
 }
