623fa4b60b002700c6ec70ec75342dca89a9a5e5
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-11-13 10:45:47 -0500

    environment

---
+++ Jenny Fu/src/javaTheHardWay/Environment.java
@@ -0,0 +1,17 @@
+package javaTheHardWay;
+
+public class Environment {
+	
+	private int plants;
+	private int growthRate;
+
+	public Environment() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
