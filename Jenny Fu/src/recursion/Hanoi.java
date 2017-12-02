c24cd11259f43beaad18a6cbb839ab6c39c682f2
jenny <jenny@DESKTOP-UHACPBF.fios-router.home>
2017-12-02 16:55:40 -0500

    a

--- Jenny Fu/src/recursion/Hanoi.java
+++ Jenny Fu/src/recursion/Hanoi.java
@@ -4,17 +4,17 @@ package recursion;
 public class Hanoi {
 
 	public static void main(String[] args) {
-		System.out.println(count(3, "start", "help", "end"));
+		System.out.println(count(4, "start", "help", "end"));
 	}
-k
+	
 	public static int count(int num, String start, String help, String end) {
 		if(num == 1)
 			return 1;
 		else if(num%2 == 0) {
-			return count(num - 1, start, end, help) + 2;
+			return count(num - 1, start, end, help);
 		}
 		else
-			return count(num - 1, start, help, end) + 2;
+			return count(num - 1, start, help, end);
 	}
 
 	//HOMEWORK:
